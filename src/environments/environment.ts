e60b215570531fd898667e0b7703629dabdb22d0
shahjahan <shahjahan.dohatec@gmail.com>
2024-08-20 01:06:51 +0600

    production urls added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 // Gateway IP
-const api_server: string = 'http://192.168.0.181:8080/api';
+const api_server: string = 'http://5.189.128.185:8080/api';
 
 export const environment = {
-  production: false,
+  production: true,
   server_root:`${api_server}`
 };
