1ff0cda7bc2283446938bb965e87cc0d559b2512
shahjahan <shahjahan.dohatec@gmail.com>
2024-12-02 10:12:18 +0600

    port added to api base url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 // Gateway IP
-const api_server: string = 'http://5.189.128.185/api';
+const api_server: string = 'http://5.189.128.185:8080/api';
 
 export const environment = {
   production: true,
